5872f8a0ae78c1513dfad54284f1953d97677b63
Zach Butler <zach.standley93@gmail.com>
2019-07-18 12:14:06 -0700

    changed default port to 7890

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7893
+const PORT = process.env.PORT || 7890
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
